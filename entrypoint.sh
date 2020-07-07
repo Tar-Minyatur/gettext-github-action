c0e516dad051eaeb5b79240d6ecebcfb9ef198d0
Till Helge Helwig <thh@tshw.de>
2020-07-07 21:37:48 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ apk add gettext
 echo "Looking for .po files in $1..."
 
 echo "Converting files..."
-find $1 -type f -name \*.po -exec echo "→ {}"; msgfmt {} \;
+find $1 -type f -name \*.po -exec "echo '→ {}'; msgfmt {}" \;
