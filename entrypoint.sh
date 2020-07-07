8c14eeeac5da150795a6cd0ad6bf8a57cd6205e3
Till Helge Helwig <thh@tshw.de>
2020-07-07 22:29:46 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,25 @@ echo "Installing Gettext..."
 apk add gettext
 
 echo "Checking .po files in $1..."
-find $1 -type f -name \*.po -print0 | xargs -0 msgfmt
+ERRORS=0
+shopt -s globstar
+for filename in "$1**.po"
+do
+  echo -n "→ $filename..."
+  msgfmt "$filename"
+  returnValue=$?
+  if [ $returnValue -ne 0 ]; then
+    ERRORS=$ERRORS+1
+    echo "contains errors!"
+  else
+    echo "ok"
+  fi
+done
+echo ""
+if [ $ERRORS -gt 1 ]; then  
+  echo "FOUND $ERRORS ERROR(S) IN THE TRANSLATION FILES. SEE ABOVE FOR DETAILS."
+  exit 1
+else
+  echo "No errors found."
+  exit 0
+fi
